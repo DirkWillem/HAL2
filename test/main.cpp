186f78e18e5ce3911ca99b89d709cba48c8a94c3
Dirk van Wijk <dirk@dirkwillem.nl>
2025-01-04 12:37:03 +0100

    WIP on vRPC

--- test/main.cpp
+++ test/main.cpp
@@ -1,26 +1,99 @@
 #include <math/geometry/transformation_matrix.h>
 #include <math/linalg/matrix.h>
 
-#include <iostream>
+#include <pb_common.h>
+
+#include <vrpc.h>
+
+#include "generated/radiation_test_setup.h"
+
+template <typename T, typename StructType, typename FieldType>
+concept FieldPointer = requires(T ptr, StructType s) {
+  { s.*ptr } -> std::convertible_to<FieldType>;
+};
+
+template <typename Msg, typename F>
+static std::optional<pb_field_iter_t>
+FindFieldByPointer(const Msg& msg, FieldPointer<Msg, F> auto field_ptr) {
+  pb_field_iter_t iter{};
+  pb_field_iter_begin_const(&iter, nanopb::MessageDescriptor<Msg>::fields(),
+                            &msg);
+
+  do {
+    if (iter.pField == &(msg.*field_ptr)) {
+      return {iter};
+    }
+  } while (pb_field_iter_next(&iter));
+
+  return {};
+}
+
+template <typename Msg, typename El>
+std::optional<std::span<const El>>
+GetRepeatedFieldFromPtr(const Msg& msg, FieldPointer<Msg, El*> auto field_ptr) {
+  const auto iter_opt = FindFieldByPointer<Msg, El*>(msg, field_ptr);
+  if (iter_opt.has_value()) {
+    const auto iter = *iter_opt;
+    if (iter.pSize == nullptr) {
+      return {};
+    }
+
+    const auto count = static_cast<std::size_t>(
+        *reinterpret_cast<const pb_size_t*>(iter.pSize));
+
+    return std::span{reinterpret_cast<const El*>(iter.pData), count};
+  }
+
+  return {};
+}
+
+template <typename PMsg, typename WMsg, FieldPointer<WMsg, uint32_t*> auto WF,
+          FieldPointer<WMsg, PMsg> auto WB>
+struct ParameterMapping {
+  static void ApplyWriteMessage(const WMsg& write_msg, PMsg& param_msg) {
+    const auto written_fields_opt =
+        GetRepeatedFieldFromPtr<WMsg, uint32_t>(write_msg, WF);
+    const auto body = write_msg.*WB;
+
+    if (!written_fields_opt.has_value()) {
+      return;
+    }
+
+    for (const auto field : *written_fields_opt) {
+      pb_field_iter_t src_iter{};
+      pb_field_iter_t dst_iter{};
+
+      const auto fields = nanopb::MessageDescriptor<PMsg>::fields();
+      pb_field_iter_begin_const(&src_iter, fields, &body);
+      pb_field_iter_begin(&dst_iter, fields, &param_msg);
+
+      if (pb_field_iter_find(&src_iter, field)
+          && pb_field_iter_find(&dst_iter, field)) {
+        std::memcpy(dst_iter.pData, src_iter.pData,
+                    std::min(src_iter.data_size, dst_iter.data_size));
+      }
+    }
+  }
+};
+
+using MyParamMap = ParameterMapping<
+    radiation_test_setup::TemperatureSensorCalibration,
+    radiation_test_setup::WriteTemperatureSensorCalibrationRequest,
+    &radiation_test_setup::WriteTemperatureSensorCalibrationRequest::fields,
+    &radiation_test_setup::WriteTemperatureSensorCalibrationRequest::
+        parameters>;
 
 int main() {
-  constexpr auto PlaneTo3D = math::Transform2DTo3DMatrix(
-      math::Vec<3, float>{0.F, 1.F, 0.F}, math::Vec<3, float>{0.F, 0.F, 1.F});
+  radiation_test_setup::TemperatureSensorCalibration             msg{};
+  radiation_test_setup::WriteTemperatureSensorCalibrationRequest w_msg{};
 
-  constexpr auto Pos   = math::Vec<2, float>{2.F, 3.F};
-  constexpr auto Pos3D = PlaneTo3D * Pos;
-  constexpr auto Pos3D_2 =
-      math::Transform2DTo3D(Pos, math::Vec<3, float>{0.F, 1.F, 0.F},
-                            math::Vec<3, float>{0.F, 0.F, 1.F});
+  w_msg.fields[0]    = 1;
+  w_msg.fields_count = 1;
 
-  constexpr math::Vec<3, float> x{1.F, 1.F, 0.F};
+  w_msg.parameters.sink_offset   = 20;
+  w_msg.parameters.source_offset = 40;
 
-  constexpr auto rot2 =
-      math::RotationMatrix3D(math::RotateZ{static_cast<float>(M_PI)});
-  constexpr auto rot = math::RotationMatrix3D(
-      math::RotateEuler{0.F, 0.F, static_cast<float>(M_PI)});
-
-  constexpr auto x2 = rot * x;
+  MyParamMap::ApplyWriteMessage(w_msg, msg);
 
   int a = 65;
 }
